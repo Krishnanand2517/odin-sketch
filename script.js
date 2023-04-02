9ac4c9167f1c51949e297d8873bf35650ae8fefe
Krishnanand2517 <krishnanand2517@gmail.com>
2023-04-02 21:41:22 +0530

    Add painting feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridSize = 16;
+let isMouseDown = false;
 const gridContainer = document.querySelector(".grid-container");
 
+// Creates all the required squares of the grid
 function createSquare(gridRow) {
     const square = document.createElement("div");
     square.classList.add("grid-square");
@@ -8,6 +10,7 @@ function createSquare(gridRow) {
     gridRow.appendChild(square);
 }
 
+// Creates each row of the grid
 function createRow() {
     const row = document.createElement("div");
     row.classList.add("grid-row");
@@ -17,6 +20,15 @@ function createRow() {
     return row;
 }
 
+// Checks if the mouse is clicked and held
+function checkMouseClick() {
+    document.body.onmousedown = (event) => { isMouseDown = true; };
+    document.body.onmouseup = (event) => { isMouseDown = false; };
+
+    return isMouseDown;
+}
+
+// Creates the grid
 for (let i = 0; i < gridSize; i++) {
     row = createRow();
     for (let j = 0; j < gridSize; j++){
@@ -24,4 +36,17 @@ for (let i = 0; i < gridSize; i++) {
     }
 }
 
+const gridSquares = document.querySelectorAll(".grid-square");
+
+// Container border
 gridContainer.style.width = gridSize * 30 + (gridSize - 2) * 2 + "px";
+
+// Painting
+gridSquares.forEach(square => square.addEventListener("mousedown", (event) => {
+    event.target.classList.add("square-drawn");
+}));
+
+gridSquares.forEach(square => square.addEventListener("mouseenter", (event) => {
+    if (!checkMouseClick()) return;
+    event.target.classList.add("square-drawn");
+}));
